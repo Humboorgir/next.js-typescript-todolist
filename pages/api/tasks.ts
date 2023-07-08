21a0df28ff4e60b875ff6d11325871ab69207396
Humboorgir <hardcraftap@gmail.com>
2023-07-08 23:49:43 +0330

    handle post requests sent to the tasks api

--- pages/api/tasks.ts
+++ pages/api/tasks.ts
@@ -11,4 +11,13 @@ export default function handler(req: NextApiRequest, res: NextApiResponse) {
   if (req.method === "GET") {
     res.status(200).json(tasks);
   }
+
+  if (req.method === "POST") {
+    const { content } = req.body;
+    if (!content) return res.status(500).send("Internal server error");
+    const id = String(Math.random());
+    const task = { id, content };
+    tasks.push(task);
+    res.status(200).json(task);
+  }
 }
