3fbc5189b51554b12b057b712add2e1d27ba12bc
Humboorgir <hardcraftap@gmail.com>
2023-07-08 22:23:21 +0330

    Create basic Todo box

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,17 @@
-const Home = () => {
-  return <div>Home</div>;
+import React from "react";
+
+import TodoList from "../components/todolist";
+
+const Home: React.FC = () => {
+  return (
+    <div className="md:container md:mx-auto px-4 flex flex-col items-center justify-center mt-20">
+      <div className="border border-neutral-700 rounded-lg p-4 text-center">
+        <h2 className="text-2xl text-slate-900">Todo list</h2>
+        <p className="text-base text-slate-700">What things are you planning to do today?</p>
+        <TodoList />
+      </div>
+    </div>
+  );
 };
 
 export default Home;
