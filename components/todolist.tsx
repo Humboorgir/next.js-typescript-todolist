288cefb88d621841c931cd44a64dad8cfedc0034
Humboorgir <hardcraftap@gmail.com>
2023-07-09 00:25:12 +0330

    Add scrollbar

--- components/todolist.tsx
+++ components/todolist.tsx
@@ -10,7 +10,7 @@ interface Props {
 
 const TodoList: React.FC<Props> = ({ tasks }) => {
   return (
-    <ul className="flex flex-col gap-2 mt-6">
+    <ul className="flex flex-col gap-2 mt-4 max-h-72 overflow-y-auto">
       {tasks.map((task) => {
         return <Task task={task} />;
       })}
